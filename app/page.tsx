bbc392dbe5185c2cf8eba6bf8f40bc7b716b39f7
Antonio Erdeljac <erdeljac.antonio@gmail.com>
2023-12-05 18:15:40 +0100

    03: auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
-import { Button } from '@/components/ui/button'
+import { UserButton } from "@clerk/nextjs"
 
 export default function Page() {
   return (
-    <Button size="lg">
-      Click me
-    </Button>
+    <div className="flex flex-col gap-y-4">
+      <h1>Dashboard</h1>
+      <UserButton
+        afterSignOutUrl="/"
+      />
+    </div>
   )
 }

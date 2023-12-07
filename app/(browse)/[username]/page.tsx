a177ad73f8bc8215b58d90e3acc6d41a0cdce949
Antonio Erdeljac <erdeljac.antonio@gmail.com>
2023-12-07 01:17:38 +0100

    12: follow service

---
+++ app/(browse)/[username]/page.tsx
@@ -0,0 +1,35 @@
+import { notFound } from "next/navigation";
+
+import { getUserByUsername } from "@/lib/user-service";
+import { isFollowingUser } from "@/lib/follow-service";
+
+import { Actions } from "./_components/actions";
+
+interface UserPageProps {
+  params: {
+    username: string;
+  };
+};
+
+const UserPage = async ({
+  params
+}: UserPageProps) => {
+  const user = await getUserByUsername(params.username);
+
+  if (!user) {
+    notFound();
+  }
+
+  const isFollowing = await isFollowingUser(user.id);
+
+  return ( 
+    <div className="flex flex-col gap-y-4">
+      <p>username: {user.username}</p>
+      <p>user ID: {user.id}</p>
+      <p>is following: {`${isFollowing}`}</p>
+      <Actions userId={user.id} isFollowing={isFollowing} />
+    </div>
+  );
+}
+ 
+export default UserPage;

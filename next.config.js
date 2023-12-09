be4eb14ef04dac8e25b6812319ff9ec87be56342
Antonio Erdeljac <erdeljac.antonio@gmail.com>
2023-12-09 19:18:35 +0100

    23: video

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.mjs$/,
+      include: /node_modules/,
+      type: "javascript/auto",
+    });
+
+    return config;
+  },
+};
 
 module.exports = nextConfig

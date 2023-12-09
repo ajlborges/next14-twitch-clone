608b8c50b6b580021621e33b8be11b25ef2444d2
Antonio Erdeljac <erdeljac.antonio@gmail.com>
2023-12-09 16:17:23 +0100

    21: livekit webhook

--- app/(browse)/_components/sidebar/following.tsx
+++ app/(browse)/_components/sidebar/following.tsx
@@ -1,13 +1,17 @@
 "use client";
 
-import { Follow, User } from "@prisma/client";
+import { Follow, Stream, User } from "@prisma/client";
 
 import { useSidebar } from "@/store/use-sidebar";
 
 import { UserItem, UserItemSkeleton } from "./user-item";
 
 interface FollowingProps {
-  data: (Follow & { following: User })[];
+  data: (Follow & { 
+    following: User & {
+      stream: Stream | null;
+    },
+  })[];
 }
 
 export const Following = ({
@@ -34,6 +38,7 @@ export const Following = ({
             key={follow.following.id}
             username={follow.following.username}
             imageUrl={follow.following.imageUrl}
+            isLive={follow.following.stream?.isLive}
           />
         ))}
       </ul>
